9e191cc282076517f3ad2b6c4cd08813f0cd5655
HAGENIMANAALPHONSE <149762207+HAGENIMANAALPHONSE@users.noreply.github.com>
2024-02-08 17:04:22 +0200

    exam project of Hospital management system

---
+++ Hospital_Management_System/src/hospital_management_system/Hospital_Management_System.java
@@ -0,0 +1,15 @@
+
+package hospital_management_system;
+
+public class Hospital_Management_System {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        login loginframe= new login();
+        loginframe.setVisible(true);
+        loginframe.pack();
+        loginframe.setLocationRelativeTo(null);
+    }
+    
+}
